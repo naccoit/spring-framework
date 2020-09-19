28af0194198096d3304c364b7bc26233545d4959
zhangchong <zhangchong@jixugou.com>
2020-09-19 12:50:53 +0800

    demo1

---
+++ spring-zveedo/src/main/java/org/springframework/veedo/model/User.java
@@ -0,0 +1,13 @@
+package org.springframework.veedo.model;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author naccoitcc
+ * @date 2020/9/19 12:44
+ */
+@Component
+public class User {
+
+	public String name = "Hello Naccoit！";
+}
